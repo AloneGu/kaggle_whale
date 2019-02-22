c2c3835b97fe4be63e09ba233d5321f45ac6bfc1
AloneGu <jackling.gu@gmail.com>
2019-02-22 22:31:30 +0800

    finished simaese baseline

--- py_scripts/model_utils.py
+++ py_scripts/model_utils.py
@@ -8,8 +8,27 @@
 """
 import os
 import keras
-from keras.layers import Input
+from keras import backend as K
+from keras.layers import Lambda, Concatenate, Dense, Flatten
+from keras.layers import Input, Conv2D, Reshape
 from keras.applications import MobileNet, ResNet50, InceptionResNetV2, Xception
+
+
+def get_callbacks(model_save_path, model=None):
+    if os.path.exists(model_save_path):
+        if model is not None:
+            model.load_weights(model_save_path, skip_mismatch=True, by_name=True)
+            print('load pre weights')
+    model_dir = os.path.dirname(model_save_path)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    m_reduce = keras.callbacks.ReduceLROnPlateau(
+        monitor='loss', factor=0.5, patience=3, verbose=1, min_lr=0.00000001)
+    m_check = keras.callbacks.ModelCheckpoint(
+        model_save_path, monitor='val_loss', verbose=1, save_best_only=True)
+    m_check2 = keras.callbacks.ModelCheckpoint(
+        model_save_path, monitor='loss', verbose=1, save_best_only=True)
+    return [m_reduce, m_check, m_check2]
 
 
 def get_mobilenet_model(img_size=224, label_cnt=5005, dense_dim=1024):
@@ -32,16 +51,51 @@ def get_mobilenet_model(img_size=224, label_cnt=5005, dense_dim=1024):
     return model
 
 
-def get_callbacks(model_save_path, model=None):
-    if os.path.exists(model_save_path):
-        if model is not None:
-            model.load_weights(model_save_path, skip_mismatch=True, by_name=True)
-            print('load pre weights')
-    model_dir = os.path.dirname(model_save_path)
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-    m_reduce = keras.callbacks.ReduceLROnPlateau(
-        monitor='loss', factor=0.5, patience=3, verbose=1, min_lr=0.00000001)
-    m_check = keras.callbacks.ModelCheckpoint(
-        model_save_path, monitor='val_loss', verbose=1, save_best_only=True)
-    return [m_reduce, m_check]
+def create_simaese_model(img_shape, mid_feat_dim=512, mid_compare_dim=128, head_model_name='mobilenet'):
+    """
+
+    :param img_shape: h, w
+    :param mid_feat_dim: middle feat shape
+    :param head_model_name: choose mobile net or others
+    :return: all_model, feat_model, compare_model
+    """
+
+    # feat model
+    img_input = Input(shape=(img_shape[0], img_shape[1], 3), name='img_feat_input')
+    if head_model_name == 'mobilenet':
+        feat_model = MobileNet(input_tensor=img_input, include_top=False, weights='imagenet', pooling='avg')
+        mid_feat = keras.layers.Dense(mid_feat_dim, name='img_feat_output')(feat_model.output)
+    else:
+        raise ValueError('head model name')
+
+    feat_model = keras.Model(img_input, mid_feat, name='top_feat')
+
+    # compare model
+    xa_inp = Input(shape=feat_model.output_shape[1:], name='cmp_1')
+    xb_inp = Input(shape=feat_model.output_shape[1:], name='cmp_2')
+    x1 = Lambda(lambda x: x[0] * x[1])([xa_inp, xb_inp])
+    x2 = Lambda(lambda x: x[0] + x[1])([xa_inp, xb_inp])
+    x3 = Lambda(lambda x: K.abs(x[0] - x[1]))([xa_inp, xb_inp])
+    x4 = Lambda(lambda x: K.square(x))(x3)
+    x = Concatenate()([x1, x2, x3, x4])
+    x = Reshape((4, feat_model.output_shape[1], 1), name='reshape1')(x)
+
+    # Per feature NN with shared weight is implemented using CONV2D with appropriate stride.
+    x = Conv2D(mid_compare_dim, (4, 1), activation='sigmoid', padding='valid')(x)
+    x = Reshape((feat_model.output_shape[1], mid_compare_dim, 1))(x)
+    x = Conv2D(1, (1, mid_compare_dim), activation='sigmoid', padding='valid')(x)
+    x = Flatten(name='flatten')(x)
+
+    # Weighted sum implemented as a Dense layer.
+    x = Dense(1, use_bias=True, activation='sigmoid', name='weighted_average')(x)
+    comp_model = keras.Model([xa_inp, xb_inp], x, name='compare')
+
+    # simaese model
+    im_a = Input(shape=(img_shape[0], img_shape[1], 3))
+    im_b = Input(shape=(img_shape[0], img_shape[1], 3))
+    im_a_feat = feat_model(im_a)
+    im_b_feat = feat_model(im_b)
+    y_out = comp_model([im_a_feat, im_b_feat])
+    model = keras.Model([im_a, im_b], y_out)
+
+    return model, feat_model, comp_model
--- py_scripts/utils.py
+++ py_scripts/utils.py
@@ -7,6 +7,7 @@
 @time: 2/13/19 20:56
 """
 import os
+import random
 import numpy as np
 import json
 import pandas as pd
@@ -39,6 +40,17 @@ class DictImageDataGenerator(ImageDataGenerator):
         return FlDictIterator(
             cls_fp_dict, self, target_size=target_size, color_mode=color_mode, class_mode=class_mode,
             batch_size=batch_size, interpolation=interpolation, shuffle=shuffle, seed=seed, use_bbox=True
+        )
+
+    def flow_from_dict_for_simaese(self, cls_fp_dict,
+                                   target_size=(256, 256), color_mode='rgb',
+                                   classes=None, class_mode='categorical',
+                                   batch_size=32, shuffle=True, seed=None,
+                                   interpolation='nearest', use_bbox=True, pos_ratio=0.5):
+        return FlSimaeseDictIterator(
+            cls_fp_dict, self, target_size=target_size, color_mode=color_mode, class_mode=class_mode,
+            batch_size=batch_size, interpolation=interpolation,
+            shuffle=shuffle, seed=seed, use_bbox=use_bbox, pos_ratio=pos_ratio
         )
 
 
@@ -170,7 +182,181 @@ class FlDictIterator(Iterator):
 
     def next(self):
         """For python 2.x.
+        # Returns
+            The next batch.
+        """
+        with self.lock:
+            index_array = next(self.index_generator)
+        # The transformation of images is not under thread lock
+        # so it can be done in parallel
+        return self._get_batches_of_transformed_samples(index_array)
 
+
+class FlSimaeseDictIterator(Iterator):
+    def __init__(self, cls_fp_dict, image_data_generator,
+                 target_size=(256, 256), color_mode='rgb',
+                 classes=None, class_mode='categorical',
+                 batch_size=32, shuffle=True, seed=None,
+                 data_format=None,
+                 save_to_dir=None,
+                 save_prefix='',
+                 save_format='png',
+                 follow_links=False,
+                 subset=None,
+                 interpolation='nearest',
+                 use_bbox=True,
+                 pos_ratio=0.5  # positive pairs in each batch
+                 ):
+        if data_format is None:
+            data_format = backend.image_data_format()
+        self.cls_fp_dict = cls_fp_dict
+        self.image_data_generator = image_data_generator
+        self.target_size = tuple(target_size)
+        if color_mode not in {'rgb', 'rgba', 'grayscale'}:
+            raise ValueError('Invalid color mode:', color_mode,
+                             '; expected "rgb", "rgba", or "grayscale".')
+        self.color_mode = color_mode
+        self.data_format = data_format
+        if self.color_mode == 'rgba':
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (4,)
+            else:
+                self.image_shape = (4,) + self.target_size
+        elif self.color_mode == 'rgb':
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (3,)
+            else:
+                self.image_shape = (3,) + self.target_size
+        else:
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (1,)
+            else:
+                self.image_shape = (1,) + self.target_size
+        self.classes = classes
+        if class_mode not in {'categorical', 'binary', 'sparse',
+                              'input', None}:
+            raise ValueError('Invalid class_mode:', class_mode,
+                             '; expected one of "categorical", '
+                             '"binary", "sparse", "input"'
+                             ' or None.')
+        self.class_mode = class_mode
+        self.save_to_dir = save_to_dir
+        self.save_prefix = save_prefix
+        self.save_format = save_format
+        self.interpolation = interpolation
+        self.pos_ratio = pos_ratio
+
+        # First, count the number of samples and classes.
+        if not classes:  # classes names
+            classes = sorted(list(self.cls_fp_dict.keys()))
+        self.num_classes = len(classes)
+        self.class_indices = dict(zip(classes, range(len(classes))))
+        self.samples = 0
+        for k in self.cls_fp_dict:
+            self.samples += len(self.cls_fp_dict[k])
+
+        print('Found %d images belonging to %d classes.' %
+              (self.samples, self.num_classes))
+
+        # Second, build an index of the images
+        # in the different class subfolders.
+        self.all_labels = list(self.cls_fp_dict.keys())
+        self.filename_to_cls_label = {}
+        self.filenames = []
+        self.classes = []
+        for k in self.cls_fp_dict:  # k is string label like new_whale
+            fp_list = self.cls_fp_dict[k]
+            cls_idx = self.class_indices[k]  # cls_idx is integer like 123
+            for i, tmp_fp in enumerate(fp_list):
+                self.filenames.append(tmp_fp)
+                self.classes.append(cls_idx)
+                self.filename_to_cls_label[tmp_fp] = k  # save label to find same or diff images
+        self.classes = np.array(self.classes)
+
+        # flag for bbox
+        self.use_bbox = use_bbox
+        if self.use_bbox:
+            print('Using bbox for image')
+
+        super(FlSimaeseDictIterator, self).__init__(self.samples,
+                                                    batch_size,
+                                                    shuffle,
+                                                    seed)
+
+    def _get_batches_of_transformed_samples(self, index_array):
+        # init x
+        batch_size = len(index_array)
+        batch_x1 = np.zeros(
+            (batch_size,) + self.image_shape,
+            dtype=backend.floatx())
+        batch_x2 = np.zeros(
+            (batch_size,) + self.image_shape,
+            dtype=backend.floatx())
+
+        # build batch of image data
+        for i, j in enumerate(index_array):
+            fname = self.filenames[j]
+            img = load_img(fname,
+                           color_mode=self.color_mode
+                           )  # resize later
+            if self.use_bbox is True:
+                base_fn = os.path.basename(fname)
+                x0, y0, x1, y1 = get_bbox(base_fn)
+                if not (x0 >= x1 or y0 >= y1):
+                    tmp_box = (x0, y0, x1, y1)
+                    img.crop(tmp_box)
+            x = img_to_array(img, data_format=self.data_format)
+            # Pillow images should be closed after `load_img`,
+            # but not PIL images.
+            if hasattr(img, 'close'):
+                img.close()
+            params = self.image_data_generator.get_random_transform(x.shape)
+            x = self.image_data_generator.apply_transform(x, params)
+            x = imresize(x, size=self.target_size, interp=self.interpolation)  # resize after random transforms
+            x = self.image_data_generator.standardize(x)
+            batch_x1[i] = x
+
+        # build next batch
+        pos_cnt = int(batch_size * self.pos_ratio)
+        for i, j in enumerate(index_array):
+            fname = self.filenames[j]
+            curr_label = self.filename_to_cls_label[fname]
+            if j <= pos_cnt:  # find same cls
+                this_label_fps = self.cls_fp_dict[curr_label]
+                fname = random.choice(this_label_fps)
+            else:  # random find another cls
+                other_labels = [l for l in self.all_labels if l != curr_label]
+                rnd_label = random.choice(other_labels)
+                other_label_fps = self.cls_fp_dict[rnd_label]
+                fname = random.choice(other_label_fps)
+            img = load_img(fname,
+                           color_mode=self.color_mode
+                           )  # resize later
+            if self.use_bbox is True:
+                base_fn = os.path.basename(fname)
+                x0, y0, x1, y1 = get_bbox(base_fn)
+                if not (x0 >= x1 or y0 >= y1):
+                    tmp_box = (x0, y0, x1, y1)
+                    img.crop(tmp_box)
+            x = img_to_array(img, data_format=self.data_format)
+            # Pillow images should be closed after `load_img`,
+            # but not PIL images.
+            if hasattr(img, 'close'):
+                img.close()
+            params = self.image_data_generator.get_random_transform(x.shape)
+            x = self.image_data_generator.apply_transform(x, params)
+            x = imresize(x, size=self.target_size, interp=self.interpolation)  # resize after random transforms
+            x = self.image_data_generator.standardize(x)
+            batch_x2[i] = x
+
+        # build batch of labels
+        batch_y = np.zeros(shape=(batch_size, 1), dtype=backend.floatx())
+        batch_y[:pos_cnt, :] = 1.0
+
+        return [batch_x1, batch_x2], batch_y
+
+    def next(self):
+        """For python 2.x.
         # Returns
             The next batch.
         """
@@ -188,7 +374,6 @@ def split_train_test_dict(all_data_d, test_rate=0.2, duplicate_low_cls=True):
         fp_list = all_data_d[k]
         fp_cnt = len(fp_list)
         split_idx = int(fp_cnt * test_rate)
-
         if split_idx == 0:  # low image count
             if duplicate_low_cls:
                 train_d[k] = fp_list
