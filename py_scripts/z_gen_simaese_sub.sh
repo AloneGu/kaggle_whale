73ef381fb5608f91bdafcad6790e1a5365e27878
AloneGu <jackling.gu@gmail.com>
2019-03-02 20:16:45 +0800

    u

--- py_scripts/z_gen_simaese_sub.sh
+++ py_scripts/z_gen_simaese_sub.sh
@@ -1,1 +1,1 @@
-python3 simaese_gen_sub.py --model_path=../data/checkpoints/mob_384_sim_hbp.h5 --train_feat=../data/simaese_img_feats/simaese_mob_384_sim_hbp.pkl
+python3 -u simaese_gen_sub.py --model_path=../data/checkpoints/mob_384_sim_hbp.h5 --train_feat=../data/simaese_img_feats/simaese_mob_384_sim_hbp.pkl
--- py_scripts/z_gen_simaese_train_feat.sh
+++ py_scripts/z_gen_simaese_train_feat.sh
@@ -1,1 +1,1 @@
-python3 simaese_img_feat_gen.py --model_path=../data/checkpoints/mob_384_sim_hbp.h5
+python3 -u simaese_img_feat_gen.py --model_path=../data/checkpoints/mob_384_sim_hbp.h5
